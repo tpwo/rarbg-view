05368edc749efee01744189361233e799b6f347e
Tomasz Wojdat <tpwojdat@gmail.com>
2023-10-28 17:07:13 +0200

    Basic loading and db reading

--- main.py
+++ main.py
@@ -1,15 +1,54 @@
 from __future__ import annotations
 
+import os
+import sqlite3
+from sqlite3 import Connection
+import more_itertools
+
 from fastapi import FastAPI
-from pydantic import BaseModel
+
+
+DB_DIR = 'db'
+DB_FILE = f'{DB_DIR}/database.db'
+
+
+def get_connection(db_file: str) -> Connection:
+    try:
+        # TODO: check_same_thread sounds risky
+        conn = sqlite3.connect(db_file, check_same_thread=False)
+    except sqlite3.OperationalError:
+        return initialize_db(db_file)
+    else:
+        return conn
+
+
+def initialize_db(db_file: str) -> Connection:
+    os.makedirs(DB_DIR, exist_ok=True)
+    open(db_file, 'w+').close()
+
+    # TODO: check_same_thread sounds risky
+    conn = sqlite3.connect(db_file, check_same_thread=False)
+
+    with open('schema/schema.sql') as file:
+        conn.executescript(file.read())
+    with open('backup/data.sql') as file:
+        # for idx, piece in enumerate(read_in_chunks(file)):
+        for idx, chunk in enumerate(more_itertools.chunked(file.readlines(), 1024)):
+            print(f'chunk {idx}')
+            conn.executescript("\n".join(chunk))
+    return conn
+
+
+def read_in_chunks(file, chunk: int = 1024):
+    while True:
+        data = file.read(chunk)
+        if not data:
+            break
+        yield data
+
 
 app = FastAPI()
-
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: bool | None = None
+CONN = get_connection(DB_FILE)
 
 
 @app.get('/')
@@ -17,11 +56,20 @@ def read_root():
     return {'Hello': 'World'}
 
 
-@app.get('/items/{item_id}')
-def read_item(item_id: int, q: str | None = None):
-    return {'item_id': item_id, 'q': q}
+@app.get('/count')
+def get_count() -> dict[str, int]:
+    with CONN as conn:
+        cursor = conn.cursor()
+        cursor.execute('SELECT COUNT(*) from items')
+        count = int(cursor.fetchone()[0])
+        return {'count': count}
 
+@app.get('/get')
+def get_count(name) -> object:
+    with CONN as conn:
+        cursor = conn.cursor()
+        cursor.execute('SELECT title, cat, dt FROM items WHERE title LIKE ? LIMIT 100', (like_str(name),))
+        return {'result': cursor.fetchall()}
 
-@app.put('/items/{item_id}')
-def update_item(item_id: int, item: Item):
-    return {'item_name': item.name, 'item_id': item_id}
+def like_str(text: str) -> str:
+    return '%' + text + '%'
