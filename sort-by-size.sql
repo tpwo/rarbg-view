897cbf5ecec38815e79d2bd008f2f343481f169c
Tomasz Wojdat <tpwojdat@gmail.com>
2025-12-25 03:42:31 +0100

    Remove old unused SQL

--- sort-by-size.sql
+++
@@ -1,7 +0,0 @@
-SELECT magnet_links.size,
-       magnetLink
-FROM magnet_links
-INNER JOIN items ON items.id = magnet_links.id
-WHERE items.cat IS NOT 'xxx'
-  AND items.title LIKE '%twin%peaks%'
-ORDER BY items.size ASC;
